a07b71dd661276020f0a5798942b44e25d96df70
Devin Dreszer <devindreszer@gmail.com>
2014-07-03 15:43:29 -0400

    Users can publicly poke some target
    * Targets have a dropdown option

--- app/controllers/pokes_controller.rb
+++ app/controllers/pokes_controller.rb
@@ -11,6 +11,7 @@ class PokesController < ApplicationController
     @poke = Poke.new(poke_params)
 
     if @poke.save
+      public_poke(@poke)
       redirect_to @poke, success: 'Target has been poked!'
     else
       flash.now.alert = "Slight problem: #{@poke.errors.full_messages.join(', ')}"
@@ -22,5 +23,10 @@ class PokesController < ApplicationController
 
   def poke_params
     params.require(:poke).permit(:author_line, :target_username, :content, :is_private)
+  end
+
+  def public_poke(poke)
+    client = HipChat::Client.new(ENV['HIPCHAT_TOKEN'], :api_version => 'v2')
+    client[Poke::ROOM].send('', "@#{poke.target_username} #{poke.content} by #{poke.author_line}: #{request.original_url}", :message_format => 'text')
   end
 end
--- app/models/poke.rb
+++ app/models/poke.rb
@@ -1,3 +1,13 @@
 class Poke < ActiveRecord::Base
+  ROOM = "WDI Boston PokeChat"
   validates :author_line, :target_username, :content, presence: true
+
+  def targets
+    client = HipChat::Client.new(ENV['HIPCHAT_TOKEN'], :api_version => 'v2')
+    participant_names = []
+    client[ROOM].get_room["participants"].each do |participant|
+      participant_names << participant["name"]
+    end
+    participant_names
+  end
 end
